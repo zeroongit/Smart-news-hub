cba8e9991c209848fb3b7ac60ecdb9ba787cdacd
zeroongit <karispramudoita8@gmail.com>
2025-06-12 01:57:34 +0700

    last update

--- smart-news-frontend/src/pages/Profile.jsx
+++ smart-news-frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getUserProfile, updateUserProfile, showMessage } from '../../services/api';
+import { getUserProfile, updateUserProfile, showMessage } from '../services/api';
 
 
 function Profile() {
