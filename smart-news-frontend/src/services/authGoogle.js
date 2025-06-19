175b9a7f20b67d831d4d879f3b1679bf5dde96a9
zeroongit <karispramudoita8@gmail.com>
2025-06-19 16:11:46 +0700

    last updated

--- smart-news-frontend/src/services/authGoogle.js
+++ smart-news-frontend/src/services/authGoogle.js
@@ -1,5 +1,5 @@
 import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from './firebaseConfig';
+import { auth, provider } from './firebase';
 
 export const signInWithGoogle = async () => {
   try {
